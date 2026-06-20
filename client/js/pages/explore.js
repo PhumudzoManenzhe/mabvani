27cff9cc49902c18547b211708c0dcf6b33d8ca8
LegadimaW <2843187@students.wits.ac.za>
2026-06-20 20:02:45 +0200

    Add files via upload

---
+++ client/js/pages/explore.js
@@ -0,0 +1,23 @@
+// Navigation tabs
+const tabs = document.querySelectorAll(".student-tab");
+const sections = document.querySelectorAll("section");
+
+// Click tab → scroll to section
+tabs.forEach(tab => {
+    tab.addEventListener("click", (e) => {
+
+        e.preventDefault();
+
+        tabs.forEach(t => t.classList.remove("is-active"));
+        tab.classList.add("is-active");
+
+        const section = document.getElementById(tab.dataset.target);
+
+        if (section) {
+            section.scrollIntoView({
+                behavior: "smooth",
+                block: "start"
+            });
+        }
+    });
+});
