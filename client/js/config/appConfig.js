236e7626b17c28393c0d31eb8496e1e1e9d5bbef
Phumudzo Manenzhe <manenzhephumudzo657@gmail.com>
2026-06-22 01:20:40 +0200

    deployment attempt4

--- client/js/config/appConfig.js
+++ client/js/config/appConfig.js
@@ -11,5 +11,5 @@ export const APP_CONFIG = Object.freeze({
     runtimeConfig.SUPABASE_ANON_KEY ||
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50dG16Ymx3YXd6bGZ6b2N5dmJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1NDcxNDQsImV4cCI6MjA5NzEyMzE0NH0.Bl9NnDhdHdegDZjia_5QyeKg9nLPIwAfHRc4nZ0pCjU",
   API_BASE_URL:
-    runtimeConfig.API_BASE_URL || "https://smartvarsity.onrender.com/api",
+    runtimeConfig.API_BASE_URL || "https://smartvarsity-api.onrender.com/api",
 });
